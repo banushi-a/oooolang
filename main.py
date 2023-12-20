e841b1122e099eb8a7c34e8feeee3a77e807cfbd
banushi-a <altonbanushi@icloud.com>
2023-12-20 17:45:24 -0600

    Added Loops + Hello World Program:

--- main.py
+++ main.py
@@ -1,50 +1,68 @@
-def process_char(char, data, pointer):
+def atomic_command(char, data, pointer):
     if char == 'O':
-        return ("", (pointer + 1) % 69_000)
+        return (pointer + 1) % 69_000
     elif char == 'o':
-        return ("", (pointer - 1) % 69_000)
+        return (pointer - 1) % 69_000
     elif char == 'Ø':
         data[pointer] += 1
-        return ("", pointer)
+        return pointer
     elif char == 'ø':
         data[pointer] -= 1
-        return ("", pointer)
-    elif char == '0':
-        pass
-    elif char == '.':
-        pass
+        return pointer
     elif char == "®":
         input_string = str(input("input: "))
         data[pointer] = ord(input_string[0])
-        return ("", pointer)
+        return pointer
     elif char == "©":
-        return (str(chr(data[pointer])), pointer)
+        print(str(chr(data[pointer])), end="")
+        return pointer
 
-    return ("", pointer)
+    return pointer
+
+def get_loop_contents(string):
+    stack = ["0"]
+    loop_contents = ""
+    for char in string[1:]:
+        if char == "0":
+            stack.append("0")
+        elif char == "." and stack[-1] == "0":
+            stack.pop()
+        elif char == ".":
+            raise ValueError("A (.) is missing a corresponding (0)")
+        
+        loop_contents += char 
+
+        if (len(stack)) == 0:
+            return loop_contents[:-1]
+        
+
+def process_string(string, data, pointer, cond, breakWhenDone):
+    loopPointer = pointer
+    while cond(loopPointer):
+        for i, char in enumerate(string):
+            if char == "0":
+                inner_loop = get_loop_contents(string[i:])
+                new_cond = lambda x: data[x] > 0
+                process_string(inner_loop, data, pointer, new_cond, False)
+            else:
+                pointer = atomic_command(char, data, pointer)
+        if breakWhenDone:
+            break
 
 def main():
     data = [0] * 69_000
-    pointer = 0
-    output = ""
 
     file_path = str(input("Enter Relative File Path: "))
 
-    # Open the file in read mode
-    with open(file_path, 'r') as file:
-        # Read the first character
-        char = file.read(1)
-
-        # Continue reading characters until the end of the file
-        while char:
-            # Process the current character
-            added_output, new_pointer = process_char(char, data, pointer)
-            output += added_output
-            pointer = new_pointer
-
-            # Read the next character
-            char = file.read(1)
-
-    print(output)
+    try:
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+            process_string(file_content, data, 0, lambda _ : True, True)
+            print()
+    except FileNotFoundError:
+        print(f"The file '{file_path}' does not exist.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
